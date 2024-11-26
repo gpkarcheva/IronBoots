128b0bc9f8bcff170a97e9738391ce72bf865061
gpkarcheva <gpkarcheva@gmail.com>
2024-11-26 13:48:10 +0200

    Added Vehicle data model.

---
+++ IronBoots.Models/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using DataAnnotationsExtensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronBoots.Data.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [Min(1)]
+        public double WeightCapacity { get; set; }
+        [Required]
+        [Min(1)]
+        public double SizeCapacity { get; set; }
+    }
+}
