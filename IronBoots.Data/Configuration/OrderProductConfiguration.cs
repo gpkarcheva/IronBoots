92313b4d209179f62bc32f4a32e19f0ca80cf1b0
Gergana K <gpkarcheva@gmail.com>
2024-11-28 01:24:19 +0200

    Added OrderProduct configuration class.

---
+++ IronBoots.Data/Configuration/OrderProductConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using IronBoots.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IronBoots.Data.Configuration
+{
+    public class OrderProductConfiguration : IEntityTypeConfiguration<OrderProduct>
+    {
+        public void Configure(EntityTypeBuilder<OrderProduct> builder)
+        {
+            builder.HasKey(op => new
+            {
+                op.OrderId,
+                op.ProductId
+            });
+
+            builder.HasOne(op => op.Order)
+                .WithMany(op => op.OrderProducts)
+                .HasForeignKey(op => op.OrderId);
+
+            builder.HasOne(po => po.Product)
+                .WithMany(po => po.ProductOrders)
+                .HasForeignKey(po => po.ProductId);
+        }
+    }
+}
