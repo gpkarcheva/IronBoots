2f17261f5839eefbf8f1cc052f753a16bb8b23f6
Gergana K <gpkarcheva@gmail.com>
2024-11-27 19:30:35 +0200

    Added OrderProduct data model.

---
+++ IronBoots.Models/OrderProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronBoots.Data.Models
+{
+    public class OrderProduct
+    {
+        [Required]
+        public Guid OrderId { get; set; }
+        [Required]
+        [ForeignKey(nameof(OrderId))]
+        public Order Order { get; set; } = null!;
+        [Required]
+        public Guid ProductId { get; set; }
+        [Required]
+        [ForeignKey(nameof(ProductId))]
+        public Product Product { get; set; } = null!;
+    }
+}
