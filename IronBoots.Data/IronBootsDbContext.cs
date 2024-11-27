9187f841424e0000a6c2fa9310dfac941e53b880
Gergana K <gpkarcheva@gmail.com>
2024-11-27 20:00:29 +0200

    Edited DbContext - added the DbSets and an override of .OnModelCreating

--- IronBoots.Data/IronBootsDbContext.cs
+++ IronBoots.Data/IronBootsDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using IronBoots.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace IronBoots.Data
@@ -8,6 +9,33 @@ namespace IronBoots.Data
         public IronBootsDbContext(DbContextOptions<IronBootsDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Town> Towns { get; set; }
+        public DbSet<Address> Adresses { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Shipment> Shipments { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<OrderProduct> OrdersProducts { get; set; }
+        public DbSet<ProductMaterial> ProductsMaterials { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<OrderProduct>()
+                .HasKey(op => new
+                {
+                    op.OrderId,
+                    op.ProductId,
+                });
+            builder.Entity<ProductMaterial>()
+                .HasKey(pm => new
+                {
+                    pm.ProductId,
+                    pm.MaterialId
+                });
         }
     }
 }
