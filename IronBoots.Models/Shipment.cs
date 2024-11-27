36edc645624ab8e82a358248cbcd54bccb2e6708
Gergana K <gpkarcheva@gmail.com>
2024-11-27 19:27:29 +0200

    Added Shipment data model.

---
+++ IronBoots.Models/Shipment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronBoots.Data.Models
+{
+    public class Shipment
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public Guid VehicleId { get; set; }
+        [Required]
+        [ForeignKey(nameof(VehicleId))]
+        public Vehicle Vehicle { get; set; } = null!;
+        [Required]
+        public DateTime ShipmentDate { get; set; }
+        [Required]
+        public DateTime DeliveryDate { get; set; }
+        public enum Status
+        {
+            PendingShipment,
+            Shipped,
+            InTransit,
+            Delivered
+        }
+    }
+}
