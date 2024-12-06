04ecb79d288705a2a744b2b8eae0198a90c6de9a
Gergana K <gpkarcheva@gmail.com>
2024-12-06 22:20:49 +0200

    Added product controller with an index method, added the view and the view model

---
+++ IronBoots/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using IronBoots.Data;
+using IronBoots.Models.Material;
+using IronBoots.Models.Products;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IronBoots.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductController(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var model = await context.Products
+                .Where(p => p.IsDeleted == false)
+                .Select(p => new ProductIndexViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    PictureUrl = p.ImageUrl
+                })
+                .AsNoTracking()
+                .ToListAsync();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details()
+        {
+
+        }
+    }
+}
---
+++ IronBoots/Models/Products/ProductIndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static IronBoots.Common.EntityValidationConstants.ProductValidation;
+
+namespace IronBoots.Models.Products
+{
+    public class ProductIndexViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [MinLength(NameMin)]
+        [MaxLength(NameMax)]
+        public string Name { get; set; } = null!;
+
+        [Url]
+        public string? PictureUrl { get; set; }
+
+        [Required]
+        public bool IsDeleted { get; set; }
+    }
+}
