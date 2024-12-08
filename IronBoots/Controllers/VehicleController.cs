3feb7fe00f9f366608ed17e080665af7957f1c8a
Gergana K <gpkarcheva@gmail.com>
2024-12-09 00:19:42 +0200

    Added Index view, added a Vehicles button to the layout, added the Index method to the VehicleController.

---
+++ IronBoots/Controllers/VehicleController.cs
@@ -0,0 +1,33 @@
+﻿using IronBoots.Data;
+using IronBoots.Models.Vehicles;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IronBoots.Controllers
+{
+    public class VehicleController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public VehicleController(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+
+        //Index
+        public async Task<IActionResult> Index()
+        {
+            List<VehicleIndexViewModel> model = await context.Vehicles
+                .Where(v => v.IsDeleted == false)
+                .Select(v => new VehicleIndexViewModel()
+                {
+                    Id = v.Id,
+                    Name = v.Name,
+                    IsAvailable = v.IsAvailable
+                })
+                .AsNoTracking()
+                .ToListAsync();
+            return View(model);
+        }
+    }
+}
