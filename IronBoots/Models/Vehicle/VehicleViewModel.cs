1e22a8aa4cb5e946ab5bc07b7fad2b8ab4913f01
Gergana K <gpkarcheva@gmail.com>
2024-12-09 00:24:37 +0200

    Added VehicleViewModel

---
+++ IronBoots/Models/Vehicle/VehicleViewModel.cs
@@ -0,0 +1,28 @@
+﻿using IronBoots.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace IronBoots.Models.Vehicle
+{
+	public class VehicleViewModel
+	{
+		[Required]
+		public Guid Id { get; set; }
+
+		[Required]
+		public string Name { get; set; } = null!;
+
+		[Required]
+		public double WeightCapacity { get; set; }
+
+		[Required]
+		public double SizeCapacity { get; set; }
+
+		public Shipment? Shipment { get; set; }
+
+		[Required]
+		public bool IsAvailable { get; set; }
+
+		[Required]
+		public bool IsDeleted { get; set; }
+	}
+}
