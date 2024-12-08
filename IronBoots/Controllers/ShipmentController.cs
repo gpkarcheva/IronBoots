4f361dac42a31f26506662bd1fd95f3db27aa000
Gergana K <gpkarcheva@gmail.com>
2024-12-08 21:11:13 +0200

    Added ShipmentController with Index method, added ShipmentIndexViewModel, and shipment index view.

---
+++ IronBoots/Controllers/ShipmentController.cs
@@ -0,0 +1,35 @@
+﻿using IronBoots.Data;
+using IronBoots.Models.Shipments;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IronBoots.Controllers
+{
+    public class ShipmentController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ShipmentController(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+
+        //Index
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<ShipmentIndexViewModel> model = await context.Shipments
+                .Where(s => s.ShipmentStatus != Data.Models.Shipment.Status.Delivered)
+                .Select(s => new ShipmentIndexViewModel()
+                {
+                    Id = s.Id,
+                    Vehicle = s.Vehicle,
+                    ShipmentStatus = (ShipmentIndexViewModel.Status)s.ShipmentStatus
+                })
+                .AsNoTracking()
+                .ToListAsync();
+
+            return View(model);
+        }
+    }
+}
---
+++ IronBoots/Models/Shipment/ShipmentIndexViewModel.cs
@@ -0,0 +1,27 @@
+﻿using IronBoots.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IronBoots.Models.Shipments
+{
+    public class ShipmentIndexViewModel
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Vehicle Vehicle { get; set; } = null!;
+
+        [Required]
+        public Status ShipmentStatus { get; set; }
+
+        public enum Status
+        {
+            PendingShipment,
+            Shipped,
+            InTransit,
+            Delivered
+        }
+    }
+}
